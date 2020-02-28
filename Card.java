b7b04430d4c67c49b7d5b5338b222259b6be0f59
ND6354 <54520924+ND6354@users.noreply.github.com>
2020-02-28 15:35:18 -0800

    Card Object

---
+++ Card.java
@@ -0,0 +1,17 @@
+import java.lang.String;
+
+public class Card {
+
+	public void setCardValue(int c) {
+		cardValue = c;
+	}
+	public void setSuit(String s) {
+		Suit = s;
+	}
+		
+private
+	int cardValue;
+	String Suit; 
+		
+}
+
