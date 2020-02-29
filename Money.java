45e0280e0c449ce44b50e9d2c95c3d414283970b
csueb401 <61036612+csueb401@users.noreply.github.com>
2020-02-28 18:05:12 -0800

    Add files via upload

---
+++ Money.java
@@ -0,0 +1,34 @@
+Public class Money {
+
+	int chips; 	// chips given to a player
+
+
+	// setting values for chips
+	public void setInitialValue(v) {
+
+	
+	}
+
+
+	// for when the player wins their money is increased
+	public void increaseMoney() {
+
+
+	}
+
+
+
+	// for when they player loses their money is decreased
+	public void decreaseMoney() {
+
+	
+	}
+
+	
+
+	// for when the player quits the game they can save whatever money they
+        //  have left from previous game	
+	public void saveMoney(Money M) {
+
+		
+	}
